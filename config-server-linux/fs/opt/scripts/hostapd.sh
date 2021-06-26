e6ded0acf24b59aaa09677a3fdc9820ff0458ac9
GongT <admin@gongt.me>
2021-06-26 11:29:48 +0800

    fix hostapd param

--- config-server-linux/fs/opt/scripts/hostapd.sh
+++ config-server-linux/fs/opt/scripts/hostapd.sh
@@ -4,4 +4,4 @@ set -Eeuo pipefail
 
 ip link set dev wlan0 up
 ip addr replace 192.168.1.1/24 dev wlan0
-exec hostapd /etc/hostapd.conf -P /var/run/config-hostapd.pid "$@"
+exec hostapd -P /tmp/hostapd.pid /etc/hostapd.conf
